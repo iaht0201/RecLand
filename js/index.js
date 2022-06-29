aee10781904b180bc30917805fba9c7d19fbd030
iaht <klrt0120@gmail.com>
2022-06-29 23:49:41 +0700

    Update Responsive

--- js/index.js
+++ js/index.js
@@ -3,9 +3,32 @@ $(".partner-list").slick({
   slidesToScroll: 1,
   dots: true,
   infinite: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        adaptiveHeight: true,
+      },
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+      },
+      // breakpoint: 308,
+      // settings: {
+      //   slidesToShow: 2,
+      //   slidesToScroll: 1,
+      //   adaptiveHeight: true,
+      // },
+    }]
 });
 $(".job-lists:first").selectable({
   filter: " > li",
+  
+ 
 });
 
 $(".search-input").on("keyup", function () {
