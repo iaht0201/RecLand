649cb073f01d3b6cd2e49d255b96fdf543aa464e
klrt0120 <klrt0120@gmail.com>
2024-08-04 16:03:12 +0700

    maintain UI

--- js/index.js
+++ js/index.js
@@ -23,18 +23,47 @@ $(".partner-list").slick({
       //   slidesToScroll: 1,
       //   adaptiveHeight: true,
       // },
-    }]
+    },
+  ],
 });
 $(".job-lists:first").selectable({
   filter: " > li",
-  
- 
 });
 
 $(".search-input").on("keyup", function () {
   var value = $(this).val().toLowerCase();
   $(".job-item").filter(function () {
     $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
-    console.log($(this))
-  }); 
+    console.log($(this));
+  });
+});
+
+$(".select-show").click(function () {
+  $(".location-list").show();
+  $(".select-show").hide();
+  $(".select-hide").show();
+});
+$(".select-hide").click(function () {
+  $(".location-list").hide();
+  $(".select-show").show();
+  $(".select-hide").hide();
+});
+
+// $(".location-list").selectable({
+//   stop: ()=>{
+//     var result = $(".location-result" ).empty();
+
+//     $(".ui-selected").each(function(){
+//       var value = $(".location-list li").text();
+//       console.log(value) ;
+//     })
+//   }
+// })
+
+$(".location-list").selectable({
+  stop: function () {
+    var select = $(".ui-selected").text();
+    console.log(select);
+    $(".location-result").val(select);
+  },
 });
